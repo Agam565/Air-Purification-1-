2bedf7e6348709d126ee691ec607869c23c157e3
Agam565 <91797164+Agam565@users.noreply.github.com>
2022-03-10 10:58:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+/*
+<!DOCTYPE html><html><head>
+  <script src="p5.min.js"></script>
+  <script src="p5.dom.min.js"></script>
+  <script src="p5.sound.min.js"></script>
+  <script src="p5.play.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+
+</head>
+
+<body>
+  <script src="sketch.js"></script>
+
+</body></html>
+
+var box;
+var airplane, airplaneImg;
+var background, backgroundImg;
+function preload(){
+  airplaneImg = loadImage("airplane.png");
+  backgroundImg = loadImage("runway.jpg");
+}
+function setup() {
+  createCanvas(400,400);
+  airplane = createSprite(200,200)
+ airplane.addImage(airplaneImg);
+ airplane.scale = 0.04;
+}
+
+function draw(){
+  background(windowWidth, windowHeight);
+  drawSprites();
+}
+//sfunction draw() 
+//{
+ //  background(30);
+
+///  if (keyIsDown(RIGHT_ARROW)) 
+ // {
+ //   box.position.x = box.position.x +5;
+ // }
+
+ //   if (keyIsDown(LEFT_ARROW)) 
+ // {
+  //  box.position.x= box.position.x -5;
+    
+ // }
+ 
+ //   if (keyIsDown(UP_ARROW)) 
+ // {
+ //   box.position.y = box.position.y - 5;
+ //  
+ // }
+
+ // if (keyIsDown(DOWN_ARROW)) 
+ // {
+  //  box.position.y = box.position.y + 5;
+  //}
+
+  //drawSprites(;
+  
+//}
+<!Doctype html>
+<head>
+
+<title>Air Purification</title>
+
+<style>
+body{
+  background-color:whitesmoke;
+}
+h1{
+  color:blue;
+  font-family:"Georgia";
+  text-align:center;
+}
+
+</style>
+</head>
+<body>
+  <h1><b><u>Air Purification</u></b></h1>
+<p> Inquiry Question
+  How do air purification systems make air healthier for people to breathe?
+  
+</p>
+</body>
+</html>*/
